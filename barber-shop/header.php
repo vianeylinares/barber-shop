4216c038bf53ce244bf41b803b44b2162ea0b504
vianey <vianeylinares@gmail.com>
2023-07-19 18:48:28 -0700

    Added header.php file.

---
+++ barber-shop/header.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * The header.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Barber Shop
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Gentlemen's Barber Shop - HTML CSS Template</title>
+
+        <!-- CSS FILES -->        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;500&display=swap" rel="stylesheet">
+
+        <link href="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/style.css" rel="stylesheet">
+<!--
+
+TemplateMo 585 Barber Shop
+
+https://templatemo.com/tm-585-barber-shop
+
+-->
+    </head>
+    
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+
+                <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <nav id="sidebarMenu" class="col-md-4 col-lg-3 d-md-block sidebar collapse p-0">
+
+                    <div class="position-sticky sidebar-sticky d-flex flex-column justify-content-center align-items-center">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/images/templatemo-barber-logo.png" class="logo-image img-fluid" align="">
+                        </a>
+
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_1">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_2">Our Story</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_3">Services</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_4">Price List</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link click-scroll" href="#section_5">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
