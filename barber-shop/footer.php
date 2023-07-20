d6860b2d537d2eddf40dfa096415176222f1fee4
vianey <vianeylinares@gmail.com>
2023-07-19 18:52:39 -0700

    Added footer.php file.

---
+++ barber-shop/footer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Barber Shop
+ */
+
+?>
+
+                <footer class="site-footer">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-12 col-12">
+                                <h4 class="site-footer-title mb-4">Our Branches</h4>
+                            </div>
+
+                            <div class="col-lg-4 col-md-6 col-11">
+                                <div class="site-footer-thumb">
+                                    <strong class="mb-1">Grünberger</strong>
+
+                                    <p>Grünberger Str. 31, 10245 Berlin, Germany</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-6 col-11">
+                                <div class="site-footer-thumb">
+                                    <strong class="mb-1">Behrenstraße</strong>
+
+                                    <p>Behrenstraße 27, 10117 Berlin, Germany</p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 col-md-6 col-11">
+                                <strong class="mb-1">Weinbergsweg</strong>
+
+                                <p>Weinbergsweg 23, 10119 Berlin, Germany</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="site-footer-bottom">
+                        <div class="container">
+                            <div class="row align-items-center">
+
+                                <div class="col-lg-8 col-12 mt-4">
+                                    <p class="copyright-text mb-0">Copyright © 2036 Barber Shop 
+                                    - Design: <a href="https://templatemo.com" rel="nofollow" target="_blank">TemplateMo</a></p>
+                                </div>
+
+                                <div class="col-lg-2 col-md-3 col-3 mt-lg-4 ms-auto">
+                                    <a href="#section_1" class="back-top-icon smoothscroll" title="Back Top">
+                                        <i class="bi-arrow-up-circle"></i>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/js/jquery.min.js"></script>
+        <script src="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/js/bootstrap.min.js"></script>
+        <script src="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/js/click-scroll.js"></script>
+        <script src="https://devbucket.xyz/barbershop/wp-content/themes/barber-shop/inc/js/custom.js"></script>
+
+    </body>
+</html>
