7e2a599b73325769f6bc5d3635109bbbab6fd055
vianey <vianeylinares@gmail.com>
2023-07-28 16:50:40 -0700

    Created basic files for Barber's pole plugin. This will handle different related post-types and data display at frontend.

---
+++ barber-pole/barber-pole.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin name: Barber's Pole
+ * Plugin URI: 
+ * Description: Barber's activities
+ * Author: VL
+ * Version: 1.0.0
+ * License: Open
+ * 
+ */
+
+//Exit if accessed directly
+if(!defined('ABSPATH')){
+       exit;
+}
