eed883396d14793cb1bab20206dfc4b2439f0766
vianey <vianeylinares@gmail.com>
2023-07-29 13:00:08 -0700

    Added metabox and custom fields to 'barber' post-type.

--- barber-pole/post-types/class.barber.php
+++ barber-pole/post-types/class.barber.php
@@ -13,6 +13,8 @@ if( ! class_exists( 'Barber' ) ){
 
         public function __construct(){
             add_action( 'init', array( $this, 'barber_register_post_type' ) );
+            add_action( 'add_meta_boxes', array( $this, 'barbers_pole_barber_metabox' ) );
+            add_action( 'save_post', array( $this,'barbers_pole_meta_save' ) );
         }
 
         // Register 'barber' post type
@@ -79,6 +81,74 @@ if( ! class_exists( 'Barber' ) ){
             );
 
             register_post_type( 'barber', $args ); 
+
+        }
+
+        public function barbers_pole_barber_metabox(){
+
+            add_meta_box(
+
+                'barbers_pole_barber_metabox',
+                'Barber social networks',
+                array( $this, 'barbers_pole_barber_callback' ),
+                'barber'
+
+            );
+
+        }
+
+        public function barbers_pole_barber_callback($post){
+
+            wp_nonce_field( basename(__FILE__), 'Barbers_Pole_enable_disable_nonce' );
+
+            $facebook = ( get_post_meta( $post->ID, 'facebook', true ) ) ? esc_html( get_post_meta( $post->ID, 'facebook', true ) ) : "" ;
+            $instagram = ( get_post_meta( $post->ID, 'instagram', true ) ) ? esc_html( get_post_meta( $post->ID, 'instagram', true ) ) : "" ;
+            $whatsapp = ( get_post_meta( $post->ID, 'whatsapp', true ) ) ? esc_html( get_post_meta( $post->ID, 'whatsapp', true ) ) : "" ;
+
+            ?>
+
+            <div>
+
+                <div id="facebook">
+                    Facebook:
+                    <div>
+                        <input type="text" name="facebook" value="<?php echo $facebook; ?>" style="width: 100%; margin-bottom: 10px;" />
+                    </div>
+                </div>
+
+                <div id="instagram">
+                    Instagram:
+                    <div>
+                        <input type="text" name="instagram" value="<?php echo $instagram; ?>" style="width: 100%; margin-bottom: 10px;" />
+                    </div>
+                </div>
+
+                <div id="whatsapp">
+                    WhatsApp:
+                    <div>
+                        <input type="text" name="whatsapp" value="<?php echo $whatsapp; ?>" style="width: 100%; margin-bottom: 10px;" />
+                    </div>
+                </div>
+
+            </div>
+
+            <?php
+
+        }
+
+        public function barbers_pole_meta_save($post_id){
+
+            $is_autosave = wp_is_post_autosave($post_id);
+            $is_revision = wp_is_post_revision($post_id);
+            $is_valid_nonce = (isset($_POST['Barbers_Pole_enable_disable_nonce']) && wp_verify_nonce($_POST['Barbers_Pole_enable_disable_nonce'], basename(__FILE__)) ) ? "true" : "false";
+
+            if($is_autosave || $is_revision || !$is_valid_nonce){
+                return;
+            }
+
+            update_post_meta( $post_id, 'facebook', $_POST['facebook'] );
+            update_post_meta( $post_id, 'instagram', $_POST['instagram'] );
+            update_post_meta( $post_id, 'whatsapp', $_POST['whatsapp'] );
 
         }
 
