98b1031c11a3c1f8c880cef49302251f4066bde5
vianey <vianeylinares@gmail.com>
2023-08-02 11:09:38 -0700

    Added 404.php file and content.

---
+++ barber-shop/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The 404 template file
+ *
+ * This is the 404 template file in a WordPress theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Barber Shop
+ */
+?>
+<?php get_header(); ?>
+	
+<main>
+    <div class="container">
+        <div class="row" style="display: block; margin-top: 100px; margin-bottom: 100px;">
+            <h1>
+                Page not found
+            </h1>
+            <p>
+                Unfortunately, the page you tried to reach does not exist on this site.
+            </p>
+        </div>
+    </div>
+</main>
+	
+<?php get_footer(); ?>
