529e46d6c5e60b9ae5084ef512d64c9c4b652c05
vianey <vianeylinares@gmail.com>
2023-08-02 10:58:29 -0700

    Changed 'show_in_nave_menu' attribute in Barber's Pole plugin post-types to 'false'. These post-types shouldn't appear as menu setup options.

--- barber-pole/post-types/class.branch.php
+++ barber-pole/post-types/class.branch.php
@@ -45,7 +45,7 @@ if( ! class_exists( 'Branch' ) ){
                     'public' => true,
                     'publicly_queryable' => true,
                     'exclude_from_search' => false,
-                    'show_in_nav_menus' => true,
+                    'show_in_nav_menus' => false,
                     'show_ui' => true,
                     'show_in_menu' => true,
                     'show_in_admin_bar' => true,
